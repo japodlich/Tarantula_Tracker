578f59986a3476f7bbed937501fa49a71536a9d3
japodlich <60197066+japodlich@users.noreply.github.com>
2020-04-14 15:02:20 -0500

    Tarantula Class

---
+++ src/main/java/model/Tarantula.java
@@ -0,0 +1,71 @@
+package model;
+
+public class Tarantula 
+{
+	private int id;
+	private String name;
+	private String species;
+	private String sizeClassification;
+	//connect molts class here?
+	
+	public Tarantula() {
+		super();
+	}
+	
+	public Tarantula(int id, String name, String species, String sizeClassification) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.species = species;
+		this.sizeClassification = sizeClassification;
+	}
+
+	public Tarantula(String name, String species, String sizeClassification) {
+		super();
+		this.name = name;
+		this.species = species;
+		this.sizeClassification = sizeClassification;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	public String getSizeClassification() {
+		return sizeClassification;
+	}
+
+	public void setSizeClassification(String sizeClassification) {
+		this.sizeClassification = sizeClassification;
+	}
+
+	@Override
+	public String toString() {
+		return "Tarantula [id=" + id + ", name=" + name + ", species=" + species + ", sizeClassification="
+				+ sizeClassification + "]";
+	}
+	
+	
+	
+	
+}
